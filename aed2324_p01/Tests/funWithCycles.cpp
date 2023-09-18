0d5155f244b57b573f515da55e73f01f2a81953b
Guilherme Coelho <gcoelho2002@gmail.com>
2023-09-18 14:13:28 +0100

    create /p01

---
+++ aed2324_p01/Tests/funWithCycles.cpp
@@ -0,0 +1,97 @@
+#include "funWithCycles.h"
+
+//=============================================================================
+// Exercise 1: Palindromes
+//=============================================================================
+// TODO
+bool FunWithCycles::palindrome(const std::string & s) {
+    return false;
+}
+
+
+//=============================================================================
+// Exercise 2: Palindrome Sentences
+//=============================================================================
+// TODO
+bool FunWithCycles::palindromeSentence(const std::string & s) {
+    return false;
+}
+
+
+//=============================================================================
+// Exercise 3: Interesting Numbers
+//=============================================================================
+// TODO
+int FunWithCycles::nextInterestingNumber(int n, int sum) {
+    return 0;
+}
+
+// This auxiliary function should return the sum of the digits of n
+// TODO
+int FunWithCycles::digitSum(int n) {
+    return 0;
+}
+
+
+//=============================================================================
+// Exercise 4: Winter is coming
+//=============================================================================
+// TODO
+int FunWithCycles::winter(const vector<int> & v) {
+    return 0;
+}
+
+
+//=============================================================================
+// Exercise 5: Playing with vectors
+//=============================================================================
+
+// a) count
+// TODO
+int FunWithCycles::count(const vector<int> & v, int n) {
+    return 0;
+}
+
+// b) hasDuplicates
+// TODO
+bool FunWithCycles::hasDuplicates(const vector<int> & v) {
+    return false;
+}
+
+// c) removeDuplicates
+// TODO
+void FunWithCycles::removeDuplicates(vector<int> & v) {
+
+}
+
+// d) merge
+// TODO
+vector<int> FunWithCycles::merge(const vector<int> & v1, const vector<int> & v2) {
+    vector<int> ans;
+    return ans;
+}
+
+
+//=============================================================================
+// Exercise 6: Playing with prime numbers
+//=============================================================================
+
+// a) isPrime
+// TODO
+bool FunWithCycles::isPrime(int n) {
+    return false;
+}
+
+// b) factorize
+// TODO
+vector<int> FunWithCycles::factorize(int n) {
+    vector<int> ans;
+    return ans;
+}
+
+// c) listPrimes
+// TODO
+vector<int> FunWithCycles::listPrimes(int n) {
+    vector<int> ans;
+    return ans;
+}
