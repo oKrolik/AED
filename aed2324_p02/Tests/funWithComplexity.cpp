1a7a55c00a233eed2fca734e3aa481fcca8b9e04
Guilherme Coelho <gcoelho2002@gmail.com>
2023-09-25 14:16:40 +0100

    Add aed2324_p02

---
+++ aed2324_p02/Tests/funWithComplexity.cpp
@@ -0,0 +1,27 @@
+#include "funWithComplexity.h"
+
+//=============================================================================
+// Exercise 4: Analyzing a river
+//=============================================================================
+// TODO
+int FunWithComplexity::river(const vector<int> & v, int k, int t) {
+    return 0;
+}
+
+//=============================================================================
+// Exercise 5: Spiral of Numbers
+//=============================================================================
+// TODO
+pair<int, int> FunWithComplexity::spiral(int n) {
+    pair<int, int> ans = {0, 0};
+    return ans;
+}
+
+
+//=============================================================================
+// Exercise 7: Squared notebook
+//=============================================================================
+// TODO
+long long FunWithComplexity::gridSum(int a, int b) {
+    return 0;
+}
