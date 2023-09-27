7fdf3b2f102a5a9ddca284376add3b54c7ea95dd
Guilherme Coelho <93000522+oKrolik@users.noreply.github.com>
2023-09-27 19:51:01 +0100

    Update funWithComplexity.cpp

--- aed2324_p02/Tests/funWithComplexity.cpp
+++ aed2324_p02/Tests/funWithComplexity.cpp
@@ -4,21 +4,18 @@
 // Exercise 4: Analyzing a river
 //=============================================================================
 // TODO
-int FunWithComplexity::river(const vector<int> & v, int k, int t) {
+int FunWithComplexity::river(const vector<int> &v, int k, int t) {
     int ans = 0;
-    for (int i = 0; i < v.size(); i++) {
-        vector<int> aux;
-        for(int j = i; j < i+k; j++) {
-            aux.push_back(v[j]);
-        }
+    for (int i = 0; i <= v.size() - k; i++) {
         int count = 0;
-        for(int z: aux) {
-            if (z > t) count++;
-        }
-        if (count > k/2) ans++;
+        for (int j = i; j < i + k; j++)
+            if (v[j] >= t) count++;
+        if ((k % 2 == 0 && count >= k / 2) || (k % 2 != 0 && count >= k / 2 + 1)) ans++;
     }
     return ans;
 }
+
+
 
 //=============================================================================
 // Exercise 5: Spiral of Numbers
@@ -26,6 +23,41 @@ int FunWithComplexity::river(const vector<int> & v, int k, int t) {
 // TODO
 pair<int, int> FunWithComplexity::spiral(int n) {
     pair<int, int> ans = {0, 0};
+
+    if (n == 1) {
+        return {0, 0}; // Caso especial para o número 1
+    }
+
+    int layer = 0;
+    int sideLength = 1;
+    int maxValueInLayer = 1;
+
+    while (maxValueInLayer < n) {
+        layer++;
+        sideLength += 2;
+        maxValueInLayer = sideLength * sideLength;
+    }
+
+    int diff = maxValueInLayer - n;
+    int halfSide = sideLength / 2;
+
+    int x, y;
+
+    if (diff < sideLength - 1) {
+        x = halfSide;
+        y = halfSide - diff;
+    } else if (diff < 2 * (sideLength - 1)) {
+        x = halfSide - (diff - (sideLength - 1));
+        y = -halfSide;
+    } else if (diff < 3 * (sideLength - 1)) {
+        x = -halfSide;
+        y = -halfSide + (diff - 2 * (sideLength - 1));
+    } else {
+        x = halfSide;
+        y = -halfSide + (diff - 3 * (sideLength - 1));
+    }
+    ans = {x, y};
+
     return ans;
 }
 
