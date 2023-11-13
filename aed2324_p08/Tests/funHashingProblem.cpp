aad37f90f3d12b59f4140292305ef61cf5a75f75
Guilherme Coelho <gcoelho2002@gmail.com>
2023-11-13 14:51:09 +0000

    add p08

---
+++ aed2324_p08/Tests/funHashingProblem.cpp
@@ -0,0 +1,17 @@
+#include "funHashingProblem.h"
+
+FunHashingProblem::FunHashingProblem() {}
+
+
+//=============================================================================
+// Exercise 1: Find Duplicates
+//=============================================================================
+// TODO
+vector<int> FunHashingProblem::findDuplicates(const vector<int>& values, int k) {
+    vector<int> res;
+    for (int i = 0; i < values.size(); i++) {
+        
+    }
+
+    return res;
+}
